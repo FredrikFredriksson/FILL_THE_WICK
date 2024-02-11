030d5e03f06926d5f8d65094633a0a236f6518d9
Fredrik Fredriksson <Fredrikfredriksson93@gmail.com>
2024-02-11 19:26:10 +0100

    removed CEO

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Home.css";
-import CEO from "../assets/FTW-CEO.png";
 import Buy from "../components/BuyToken/Buy";
 import Info from "../components/Info/Info";
 import Header from "../components/Header/Header";
